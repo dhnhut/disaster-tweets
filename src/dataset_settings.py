41516908210d22ccac90558c5ed7fd32795aa768
dhnhut@gmail.com <dhnhut@gmail.com>
2026-03-26 00:59:52 +1300

    Add dataset col

--- src/dataset_settings.py
+++ src/dataset_settings.py
@@ -1,6 +1,8 @@
-dataset_cols = ["tweet_id", "tweet_text", "label", "event_type"]
+dataset_cols = ["tweet_id", "tweet_text", "relevant", "event_type"]
 
 dataset_extended_cols = dataset_cols + [
+    "dataset",
+    "original_label",
     "country",
     "year",
     "meta",
