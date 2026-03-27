b71b9005ee4f6e892a8eaa2e665f08cf890e8b8f
dhnhut@gmail.com <dhnhut@gmail.com>
2026-03-27 23:41:28 +1300

    Unify datasets to one

--- src/dataset_settings.py
+++ src/dataset_settings.py
@@ -1,6 +1,7 @@
 dataset_cols = ["tweet_id", "tweet_text", "relevant", "event_type"]
 
 dataset_extended_cols = dataset_cols + [
+    "event_name",
     "dataset",
     "original_label",
     "country",
