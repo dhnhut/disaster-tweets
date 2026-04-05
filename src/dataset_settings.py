2c489541d73681805f4fd358bcf8b72da490904b
dhnhut@gmail.com <dhnhut@gmail.com>
2026-04-05 13:43:10 +1200

    Update unified dataset columns

--- src/dataset_settings.py
+++ src/dataset_settings.py
@@ -1,10 +1,16 @@
-dataset_cols = ["tweet_id", "tweet_text", "relevant", "event_type"]
+dataset_cols = [
+    "tweet_id",
+    "tweet_text",
+    "relevant",
+    "humanitarian_label",
+    "event_type",
+]
 
 dataset_extended_cols = dataset_cols + [
     "event_name",
     "dataset",
-    "original_label",
-    "country",
+    "original_relevant_label",
+    "original_humanitarian_label",
     "year",
     "meta",
 ]
