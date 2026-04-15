c26665a8f240cdb63604cff5529642e9c2abf324
dhnhut@gmail.com <dhnhut@gmail.com>
2026-04-15 14:52:09 +1200

    Align Dataset names

--- src/dataset_settings.py
+++ src/dataset_settings.py
@@ -14,3 +14,20 @@ dataset_extended_cols = dataset_cols + [
     "year",
     "meta",
 ]
+
+# dataset_names = [
+#     "ACL_ICWSM_2018",
+#     "AIDR_system",
+#     "CrisisLexT26",
+#     "CrisisLexT6",
+#     "CrisisMMD",
+#     "CrisisNLP-CF",
+#     "CrisisNLP-volunteers",
+#     "DRD-FigureEight-Multimedia",
+#     "DSM-CF",
+#     "Ecuador_Earthquake_2016",
+#     "HumAID",
+#     "ISCRAM13",
+#     "SWDM13",
+#     "eyewitness_messages",
+# ]
