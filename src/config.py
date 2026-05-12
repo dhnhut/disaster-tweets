0260ded478c96e369d4b94c9ba71b343bd4a4d24
dhnhut@gmail.com <dhnhut@gmail.com>
2026-05-13 11:34:23 +1200

    Random spliting train/vali/test sets

--- src/config.py
+++ src/config.py
@@ -18,3 +18,15 @@ DATA_FRACTION = 0.1
 DATA_DISASTER_FRACTION = 0.30
 DATA_WEATHER_FRACTION = 0.25
 DATA_OUT_TOPIC_FRACTION = 0.015
+
+
+def get_data_disaster_fraction():
+    return DATA_DISASTER_FRACTION * DATA_FRACTION
+
+
+def get_data_weather_fraction():
+    return DATA_WEATHER_FRACTION * DATA_FRACTION
+
+
+def get_data_out_topic_fraction():
+    return DATA_OUT_TOPIC_FRACTION * DATA_FRACTION
