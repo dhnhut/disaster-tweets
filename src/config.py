c5c1782ad7782e3426c98913809041f3aa1efd15
dhnhut@gmail.com <dhnhut@gmail.com>
2026-05-13 02:29:53 +1200

    Export extendeds datasets

--- src/config.py
+++ src/config.py
@@ -1,6 +1,20 @@
 RANDOM_SEED = 42
 
 # Fraction of the dataset to use for training and evaluation
-# 0.1, aka 10%, for quick testing
-# 1.0, aka 100%, for the whole dataset
-DB_FRACTION = 0.1
+
+# # FULL
+# # ORIGINAL DATASET COUNTS:
+# # Disaster	TRUE	170040	244,403     1.74%
+# # 	        FALSE	74363               0.76%
+# # Weather			            51,033      0.52%
+# # Out Topic			          9,449,777   96.97%
+
+# LOCAL
+DATA_FRACTION = 0.1
+
+# # CLOUD
+# DATA_FRACTION = 1.0
+
+DATA_DISASTER_FRACTION = 0.30
+DATA_WEATHER_FRACTION = 0.25
+DATA_OUT_TOPIC_FRACTION = 0.015
