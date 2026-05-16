3f7d9d21b49b9575091379353d6b2a179b1eb790
nduo221 <nduo221@aucklanduni.ac.nz>
2026-05-16 13:51:35 +1200

    similarity using lsh

--- notebooks/configuration.py
+++ notebooks/configuration.py
@@ -2,8 +2,7 @@ from IPython import get_ipython
 import sys
 import os
 
-
 ipython = get_ipython()
-ipython.magic("load_ext autoreload")
-ipython.magic("autoreload 2")
+# ipython.magic("load_ext autoreload")
+# ipython.magic("autoreload 2")
 sys.path.append(os.path.abspath(os.path.join("..")))
