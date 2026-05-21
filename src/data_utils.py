652185de40fb7224ebc95fffc3aa4080979f6b22
dhnhut@gmail.com <dhnhut@gmail.com>
2026-05-22 01:35:16 +1200

    run multiple BERT baseline

--- src/data_utils.py
+++ src/data_utils.py
@@ -33,6 +33,10 @@ EXPERIMENT_RATIOS = [
     [1, 0.0876],
 ]
 
+INFORMATIVE_FILE = "df_disaster_informative_knearest_0.75_100.csv"
+WEATHER_FILE = "df_weather_radius_0.75.csv"
+OUT_TOPIC_FILE = "df_out_topic_knearest_0.6_top_100.csv"
+
 
 def get_data_fraction():
     return DATA_FRACTION
@@ -50,25 +54,57 @@ def get_data_out_topic_fraction():
     return DATA_OUT_TOPIC_FRACTION * DATA_FRACTION
 
 
+def get_experiment_ratios_path(weather_ratio, out_topic_ratio):
+    return Path(f"w{weather_ratio}_o{out_topic_ratio}") / str(get_data_fraction())
+
+
 def get_data_path(type: str = ""):
     path = Path(f"../data/{type}")
     if type == "disaster":
-        path = Path(f"../data/splited/{get_data_disaster_fraction()}")
+        return path / f"../data/splited/{get_data_disaster_fraction()}"
     elif type == "weather":
-        path = Path(f"../data/splited/{get_data_weather_fraction()}")
+        return path / f"../data/splited/{get_data_weather_fraction()}"
     elif type == "out_topic":
-        path = Path(f"../data/splited/{get_data_out_topic_fraction()}")
+        return path / f"../data/splited/{get_data_out_topic_fraction()}"
+    else:
+        return path
 
-    return Path(f"../data/{type}")
 
-
-def get_data_set(set_name, label="informative", frac: float = get_data_fraction()):
-    path = get_data_path("splited") / str(frac)
+def get_data_set(
+    set_name,
+    label="informative",
+    weather_ratio=None,
+    out_topic_ratio=None,
+):
+    path = get_data_path("splited") / get_experiment_ratios_path(
+        weather_ratio, out_topic_ratio
+    )
     return path / f"{label}_{set_name}.csv"
 
 
-def load_datasets(label="informative", frac: float = get_data_fraction()):
-    df_train = pd.read_csv(get_data_set("train", label=label, frac=frac))
-    df_val = pd.read_csv(get_data_set("validation", label=label, frac=frac))
-    df_test = pd.read_csv(get_data_set("test", label=label, frac=frac))
+def load_datasets(label="informative", weather_ratio=None, out_topic_ratio=None):
+    df_train = pd.read_csv(
+        get_data_set(
+            "train",
+            label=label,
+            weather_ratio=weather_ratio,
+            out_topic_ratio=out_topic_ratio,
+        )
+    )
+    df_val = pd.read_csv(
+        get_data_set(
+            "validation",
+            label=label,
+            weather_ratio=weather_ratio,
+            out_topic_ratio=out_topic_ratio,
+        )
+    )
+    df_test = pd.read_csv(
+        get_data_set(
+            "test",
+            label=label,
+            weather_ratio=weather_ratio,
+            out_topic_ratio=out_topic_ratio,
+        )
+    )
     return df_train, df_val, df_test
